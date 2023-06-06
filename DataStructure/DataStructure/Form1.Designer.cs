61c3429f41911334433e40e7783f0837ffa403e5
Polina <91316935+Polinalavender@users.noreply.github.com>
2023-06-06 06:50:10 -0700

    add full GUI for project

---
+++ DataStructure/DataStructure/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace DataStructure
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label4 = new Label();
+            label5 = new Label();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            button4 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkOrchid;
+            label1.Location = new Point(312, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(185, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Data Structures ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 118);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(259, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(70, 256);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(307, 139);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.DarkViolet;
+            button1.Location = new Point(286, 118);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 27);
+            button1.TabIndex = 4;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Enter the input:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 159);
+            label3.Name = "label3";
+            label3.Size = new Size(156, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Choose data structure:";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(12, 198);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(82, 24);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Hashset";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(135, 198);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(99, 24);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Linked List";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(260, 198);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(87, 24);
+            radioButton3.TabIndex = 9;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "ArrayList";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 240);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Result:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(421, 83);
+            label5.Name = "label5";
+            label5.Size = new Size(122, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Sort algorithms:";
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(421, 121);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(105, 24);
+            radioButton4.TabIndex = 12;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Bucket Sort";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(421, 159);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(108, 24);
+            radioButton5.TabIndex = 13;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Bubble Sort";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(421, 293);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(421, 383);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(552, 291);
+            button2.Name = "button2";
+            button2.Size = new Size(41, 29);
+            button2.TabIndex = 16;
+            button2.Text = "go!";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(552, 381);
+            button3.Name = "button3";
+            button3.Size = new Size(41, 29);
+            button3.TabIndex = 17;
+            button3.Text = "go!";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(421, 217);
+            label6.Name = "label6";
+            label6.Size = new Size(139, 20);
+            label6.TabIndex = 18;
+            label6.Text = "Search algorithms:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(421, 256);
+            label7.Name = "label7";
+            label7.Size = new Size(99, 20);
+            label7.TabIndex = 19;
+            label7.Text = "Binary search:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(421, 343);
+            label8.Name = "label8";
+            label8.Size = new Size(98, 20);
+            label8.TabIndex = 20;
+            label8.Text = "Linear search:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(657, 34);
+            label9.Name = "label9";
+            label9.Size = new Size(131, 20);
+            label9.TabIndex = 21;
+            label9.Text = "Time of execution:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(688, 69);
+            label10.Name = "label10";
+            label10.Size = new Size(58, 20);
+            label10.TabIndex = 22;
+            label10.Text = "label10";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(283, 409);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 23;
+            button4.Text = "Desc order";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(70, 409);
+            button6.Name = "button6";
+            button6.Size = new Size(100, 29);
+            button6.TabIndex = 25;
+            button6.Text = "Asc order";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button4);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Label label4;
+        private Label label5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button2;
+        private Button button3;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Button button4;
+        private Button button6;
+    }
+}
